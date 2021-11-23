344b972718a18ad268783fc4b54ad40cd951a5c5
jt326 <91534866+jt326@users.noreply.github.com>
2021-11-23 12:06:07 +0000

    Update Geometry.hh
    Added rock daughter volume within rock layer.

--- full_geom_v1/include/Geometry.hh
+++ full_geom_v1/include/Geometry.hh
@@ -61,7 +61,7 @@ namespace Geometry {
     const double TPC_D = 3.5 * m; // diameter
     // position - active volume centered
     // making this to be calculated from the distance of WT from the wall //const double TPC_X = 0.;
-    const double WT_FROM_WALL_OFFSET = 5*m;
+    const double WT_FROM_WALL_OFFSET = 5 * m;
     const double TPC_Y = 0.;
     const double TPC_Z = 0.;
     // fix X position of the hall to 0
@@ -69,16 +69,16 @@ namespace Geometry {
 
 
     // reflective cover
-    const G4String  PTFE_MATERIAL  = "PTFE";
-    const double PTFE_THICKNESS = 3*cm;
+    const G4String  PTFE_MATERIAL = "PTFE";
+    const double PTFE_THICKNESS = 3 * cm;
 
     // reverse field region
     const G4String RFR_MATERIAL = "LXe";
-    const double RFR_H       = 20*cm;
+    const double RFR_H = 20 * cm;
 
     // approximation of PMT array
     const G4String PMT_ARR_MATERIAL = "Steel5";
-    const double PMT_ARR_H = 40*cm;
+    const double PMT_ARR_H = 40 * cm;
 
     // TPC envelope volume to hold volumes described above; should
     // fill ~0.5 mm space between volumes so that they do not touch
@@ -86,12 +86,12 @@ namespace Geometry {
 
     // outer Xe skin
     const G4String SKIN_MATERIAL = "LXe";
-    const double SKIN_THICK_SIDE = 8*cm;
-    const double SKIN_THICK_BOTTOM = 70*cm;
+    const double SKIN_THICK_SIDE = 8 * cm;
+    const double SKIN_THICK_BOTTOM = 70 * cm;
 
     // top dome
     const G4String DOME_MATERIAL = "GXe";
-    const double DOME_HH = 50*cm; // height above top PMT array
+    const double DOME_HH = 50 * cm; // height above top PMT array
 
     // Inner detector envelope - to hold skin and dome volumes; should
     // fill ~0.5 mm space between volumes so that they do not touch
@@ -99,45 +99,50 @@ namespace Geometry {
 
     // inner cryostat vessel
     const G4String ICV_MATERIAL = "Titanium";
-    const double ICV_THICKNESS = 2*cm;
+    const double ICV_THICKNESS = 2 * cm;
 
     // cryo vacuum
     const G4String VAC_MATERIAL = "Vacuum";
-    const double VAC_THICKNESS = 5*cm;
+    const double VAC_THICKNESS = 5 * cm;
 
     // outer cryostat vessel
     const G4String  OCV_MATERIAL = "Titanium";
-    const double OCV_THICKNESS = 2*cm;
+    const double OCV_THICKNESS = 2 * cm;
 
 
     // Scintillator - LS, LAB, Gd
     const G4String  GDLS_MATERIAL = "GdLS";
-    const double GDLS_THICKNESS = 50*cm;
+    const double GDLS_THICKNESS = 50 * cm;
 
     // Water tank
     const G4String  WT_MATERIAL = "Water";
-    const double WT_THICKNESS_TOP = 3.5*m;
-    const double WT_THICKNESS_BOTTOM = 1.5*m;
-    const double WT_THICKNESS_SIDE = 3.5*m;//4*m;
+    const double WT_THICKNESS_TOP = 3.5 * m;
+    const double WT_THICKNESS_BOTTOM = 1.5 * m;
+    const double WT_THICKNESS_SIDE = 3.5 * m;//4*m;
 
     // Bottom steel shielding
     const G4String  STEEL_MATERIAL = "Steel";
-    const double STEEL_H = 30*cm;
-    const double STEEL_OVERLAP = 1*m;
+    const double STEEL_H = 30 * cm;
+    const double STEEL_OVERLAP = 1 * m;
 
     // Hall
     const G4String  HALL_MATERIAL = "Air";
-    const double HALL_D = 30*m;
-    const double HALL_H = 30*m;
+    const double HALL_D = 30 * m;
+    const double HALL_H = 30 * m;
     //const double HALL_SPACE_TOP = 8*m;
     //const double HALL_SPACE_BOTTOM = 1*m;
     //const double HALL_SPACE_SIDE = 3*m;
 
-    // Rock
+    //Rock daughter volume
     const G4String  ROCK_MATERIAL = "Rock";
-    const double ROCK_SPACE_TOP = 7*m;
-    const double ROCK_SPACE_SIDE = 5*m;
-    const double ROCK_SPACE_BOTTOM = 3*m;
+    const double ROCK_DAUGHTER_TOP = 0.5 * m;
+    const double ROCK_DAUGHTER_BOTTOM = 0.5 * m;
+    const double ROCK_DAUGHTER_SIDE = 0.5 * m;
+    // Rock
+    // const G4String  ROCK_MATERIAL = "Rock";
+    const double ROCK_SPACE_TOP = 7 * m;
+    const double ROCK_SPACE_SIDE = 5 * m;
+    const double ROCK_SPACE_BOTTOM = 3 * m;
 
 
     // World
@@ -146,12 +151,12 @@ namespace Geometry {
 
     // external Xe container
     const G4String  PE_MATERIAL = "PE";
-    const double    EXTERNAL_PE_THICKNESS = 20*cm;
+    const double    EXTERNAL_PE_THICKNESS = 20 * cm;
 
     const G4String  EXTERNAL_XE_MATERIAL = "Xe_External";
-    const double EXTERNAL_XE_D = 221*cm;
-    const double EXTERNAL_XE_H = 221*cm;
-    const double EXTERNAL_XE_WALL_X_OFFSET = 2*m;
+    const double EXTERNAL_XE_D = 221 * cm;
+    const double EXTERNAL_XE_H = 221 * cm;
+    const double EXTERNAL_XE_WALL_X_OFFSET = 2 * m;
 
     const double EXTERNAL_XE_Y = 0.;
     const double EXTERNAL_XE_Z = 0.;
@@ -159,7 +164,7 @@ namespace Geometry {
 
     // helper variable
     // envelope spacing
-    const double ENV_SPACING = 0.5*mm;
+    const double ENV_SPACING = 0.5 * mm;
 
 
     /// ========== Derived properties ==========
@@ -168,13 +173,13 @@ namespace Geometry {
     // do dimensions first
     // bottom PTFE plate
     const double PTFE_B_H = PTFE_THICKNESS;
-    const double PTFE_B_D = TPC_D + 2*PTFE_THICKNESS;
+    const double PTFE_B_D = TPC_D + 2 * PTFE_THICKNESS;
     // top PTFE plate
     const double PTFE_T_H = PTFE_THICKNESS;
-    const double PTFE_T_D = TPC_D + 2*PTFE_THICKNESS;
+    const double PTFE_T_D = TPC_D + 2 * PTFE_THICKNESS;
     // side PTFE tube
     const double PTFE_S_D_INNER = TPC_D;
-    const double PTFE_S_D_OUTER = TPC_D + 2*PTFE_THICKNESS;
+    const double PTFE_S_D_OUTER = TPC_D + 2 * PTFE_THICKNESS;
     const double PTFE_S_H = TPC_H + RFR_H;
     // RFR cylinder
     const double RFR_D = TPC_D;
@@ -185,7 +190,7 @@ namespace Geometry {
     const double TPC_ENV_D = PTFE_S_D_OUTER;
     // skin layer (cylinder)
     const double SKIN_H = TPC_ENV_H + SKIN_THICK_BOTTOM;
-    const double SKIN_D = TPC_ENV_D + 2*SKIN_THICK_SIDE;
+    const double SKIN_D = TPC_ENV_D + 2 * SKIN_THICK_SIDE;
     // top dome (cylinder)
     //
     // stretches from top level of LXe, contains
@@ -197,82 +202,85 @@ namespace Geometry {
     const double ID_ENV_D = SKIN_D;
     const double ID_ENV_H = SKIN_H + DOME_H;
     // ICV
-    const double ICV_D = ID_ENV_D + 2*ICV_THICKNESS;
-    const double ICV_H = ID_ENV_H + 2*ICV_THICKNESS;
+    const double ICV_D = ID_ENV_D + 2 * ICV_THICKNESS;
+    const double ICV_H = ID_ENV_H + 2 * ICV_THICKNESS;
     // Cryo vacuum
-    const double VAC_D = ICV_D + 2*VAC_THICKNESS;
-    const double VAC_H = ICV_H + 2*VAC_THICKNESS;
+    const double VAC_D = ICV_D + 2 * VAC_THICKNESS;
+    const double VAC_H = ICV_H + 2 * VAC_THICKNESS;
     /// OCV
-    const double OCV_D = VAC_D + 2*OCV_THICKNESS;
-    const double OCV_H = VAC_H + 2*OCV_THICKNESS;
+    const double OCV_D = VAC_D + 2 * OCV_THICKNESS;
+    const double OCV_H = VAC_H + 2 * OCV_THICKNESS;
     /// GdLS - scintillator around the cryostat
-    const double GDLS_D = OCV_D + 2*GDLS_THICKNESS;
-    const double GDLS_H = OCV_H + 2*GDLS_THICKNESS;
+    const double GDLS_D = OCV_D + 2 * GDLS_THICKNESS;
+    const double GDLS_H = OCV_H + 2 * GDLS_THICKNESS;
     /// Water tank
-    const double WT_D = GDLS_D + 2*WT_THICKNESS_SIDE;
+    const double WT_D = GDLS_D + 2 * WT_THICKNESS_SIDE;
     const double WT_H = GDLS_H + WT_THICKNESS_TOP + WT_THICKNESS_BOTTOM;
     // Bottom steel shielding
-    const double STEEL_D = GDLS_D + 2*STEEL_OVERLAP;
+    const double STEEL_D = GDLS_D + 2 * STEEL_OVERLAP;
+    // Rock daughter volume
+    const double ROCK_DAUGHTER_W = HALL_D + 2 * ROCK_DAUGHTER_SIDE;
+    const double ROCK_DAUGHTER_H = HALL_H + ROCK_DAUGHTER_TOP + ROCK_DAUGHTER_BOTTOM;
     // Rock
-    const double ROCK_W = HALL_D + 2*ROCK_SPACE_SIDE;
+    const double ROCK_W = HALL_D + 2 * ROCK_SPACE_SIDE;
     const double ROCK_H = HALL_H + ROCK_SPACE_TOP + ROCK_SPACE_BOTTOM;
     // World
-    const double WORLD_W = 2*ROCK_W;
-    const double WORLD_H = 2*ROCK_H;
+    const double WORLD_W = 2 * ROCK_W;
+    const double WORLD_H = 2 * ROCK_H;
 
     // External Xe Tank
-    const double EXTERNAL_PE_D = EXTERNAL_XE_D + 2.*EXTERNAL_PE_THICKNESS;
-    const double EXTERNAL_PE_H = EXTERNAL_XE_H + 2.*EXTERNAL_PE_THICKNESS;
+    const double EXTERNAL_PE_D = EXTERNAL_XE_D + 2. * EXTERNAL_PE_THICKNESS;
+    const double EXTERNAL_PE_H = EXTERNAL_XE_H + 2. * EXTERNAL_PE_THICKNESS;
 
 
 
     // X offset of TPC
-    const double TPC_X = 0. - 0.5*HALL_D + WT_FROM_WALL_OFFSET + 0.5*WT_D;
+    const double TPC_X = 0. - 0.5 * HALL_D + WT_FROM_WALL_OFFSET + 0.5 * WT_D;
 
     // positioning in absolute coordinates (TPC placed at 0.)
     const double PTFE_B_X = TPC_X;
     const double PTFE_B_Y = TPC_Y;
-    const double PTFE_B_Z = TPC_Z - 0.5*TPC_H - RFR_H - 0.5*PTFE_THICKNESS;
+    const double PTFE_B_Z = TPC_Z - 0.5 * TPC_H - RFR_H - 0.5 * PTFE_THICKNESS;
 
 
     const double PTFE_T_X = TPC_X;
     const double PTFE_T_Y = TPC_Y;
-    const double PTFE_T_Z = TPC_Z + 0.5*TPC_H + 0.5*PTFE_THICKNESS;
+    const double PTFE_T_Z = TPC_Z + 0.5 * TPC_H + 0.5 * PTFE_THICKNESS;
 
     const double PTFE_S_X = TPC_X;
     const double PTFE_S_Y = TPC_Y;
-    const double PTFE_S_Z = TPC_Z - 0.5*RFR_H; // off-set by RFR
+    const double PTFE_S_Z = TPC_Z - 0.5 * RFR_H; // off-set by RFR
 
 
     const double RFR_X = TPC_X;
     const double RFR_Y = TPC_Y;
-    const double RFR_Z = TPC_Z - 0.5*TPC_H - 0.5*RFR_H;
+    const double RFR_Z = TPC_Z - 0.5 * TPC_H - 0.5 * RFR_H;
 
     // top PMT array
     const double PMT_T_X = TPC_X;
     const double PMT_T_Y = TPC_Y;
-    const double PMT_T_Z = TPC_Z + 0.5*TPC_H + PTFE_T_H + 0.5*PMT_ARR_H;
+    const double PMT_T_Z = TPC_Z + 0.5 * TPC_H + PTFE_T_H + 0.5 * PMT_ARR_H;
     // bottom PMT array
     const double PMT_B_X = TPC_X;
     const double PMT_B_Y = TPC_Y;
-    const double PMT_B_Z = PTFE_B_Z - 0.5*PTFE_B_H - 0.5*PMT_ARR_H;
+    const double PMT_B_Z = PTFE_B_Z - 0.5 * PTFE_B_H - 0.5 * PMT_ARR_H;
 
     const double TPC_ENV_X = TPC_X;
     const double TPC_ENV_Y = TPC_Y;
-    const double TPC_ENV_Z = PTFE_S_Z - 0.5*PMT_ARR_H - 0.5*PTFE_B_H;
+    const double TPC_ENV_Z = PTFE_S_Z - 0.5 * PMT_ARR_H - 0.5 * PTFE_B_H;
 
 
     const double SKIN_X = TPC_X;
     const double SKIN_Y = TPC_Y;
-    const double SKIN_Z = TPC_ENV_Z - 0.5*SKIN_THICK_BOTTOM;
+    const double SKIN_Z = TPC_ENV_Z - 0.5 * SKIN_THICK_BOTTOM;
 
     const double DOME_X = TPC_X;
     const double DOME_Y = TPC_Y;
-    const double DOME_Z = TPC_ENV_Z + 0.5*TPC_ENV_H + 0.5*DOME_H;
+    const double DOME_Z = TPC_ENV_Z + 0.5 * TPC_ENV_H + 0.5 * DOME_H;
 
     const double ID_ENV_X = TPC_X;
     const double ID_ENV_Y = TPC_Y;
-    const double ID_ENV_Z = SKIN_Z + 0.5*DOME_H;
+    const double ID_ENV_Z = SKIN_Z + 0.5 * DOME_H;
 
     const double ICV_X = TPC_X;
     const double ICV_Y = TPC_Y;
@@ -292,11 +300,11 @@ namespace Geometry {
 
     const double WT_X = TPC_X;
     const double WT_Y = TPC_Y;
-    const double WT_Z = GDLS_Z + 0.5*WT_THICKNESS_TOP - 0.5*WT_THICKNESS_BOTTOM;
+    const double WT_Z = GDLS_Z + 0.5 * WT_THICKNESS_TOP - 0.5 * WT_THICKNESS_BOTTOM;
 
     const double STEEL_X = TPC_X;
     const double STEEL_Y = TPC_Y;
-    const double STEEL_Z = WT_Z - 0.5*WT_H - 0.5*STEEL_H;
+    const double STEEL_Z = WT_Z - 0.5 * WT_H - 0.5 * STEEL_H;
 
 
     /// Hall
@@ -304,37 +312,41 @@ namespace Geometry {
     // const double HALL_H = WT_H + HALL_SPACE_TOP + STEEL_H;
     //const double HALL_X = TPC_X;
     const double HALL_Y = WT_Y;
-    const double HALL_Z = WT_Z - 0.5*WT_H - STEEL_H + 0.5*HALL_H;
+    const double HALL_Z = WT_Z - 0.5 * WT_H - STEEL_H + 0.5 * HALL_H;
+
+    const double ROCK_DAUGHTER_X = HALL_X;
+    const double ROCK_DAUGHTER_Y = HALL_Y;
+    const double ROCK_DAUGHTER_Z = HALL_Z + 0.5 * ROCK_DAUGHTER_TOP - 0.5 * ROCK_DAUGHTER_BOTTOM;
 
     const double ROCK_X = HALL_X;
     const double ROCK_Y = HALL_Y;
-    const double ROCK_Z = HALL_Z + 0.5*ROCK_SPACE_TOP - 0.5*ROCK_SPACE_BOTTOM;
+    const double ROCK_Z = HALL_Z + 0.5 * ROCK_SPACE_TOP - 0.5 * ROCK_SPACE_BOTTOM;
 
 
     // External Xe tank
-    const double EXTERNAL_PE_X = HALL_X + 0.5*HALL_D - EXTERNAL_XE_WALL_X_OFFSET - 0.5*EXTERNAL_PE_D;
+    const double EXTERNAL_PE_X = HALL_X + 0.5 * HALL_D - EXTERNAL_XE_WALL_X_OFFSET - 0.5 * EXTERNAL_PE_D;
     const double EXTERNAL_PE_Y = EXTERNAL_XE_Y;
     const double EXTERNAL_PE_Z = EXTERNAL_XE_Z;
     const double EXTERNAL_XE_X = EXTERNAL_PE_X;
 
     // Definition of materials
 
-    const std::map<const G4String,const G4String> material_map = {
-	{"LXe", "G4_lXe"},
-	{"PTFE", "PTFE"},
-	{"Steel5", "Steel5"},
-	{"Vacuum", "G4_Galactic"},
-	{"GXe", "G4_Xe"},
-	{"Titanium", "G4_Ti"},
-	{"Air", "G4_AIR"},
-	// {"Rock", "StdRock"},
-	//{"Rock", "NaCl"},
-	{"Rock", "polyhalite"},
-	{"Water", "G4_WATER"},
-	{"GdLS", "GdLS"},
-	{"Steel", "G4_STAINLESS-STEEL"},
-	{"PE", "G4_POLYETHYLENE"},
-	{"Xe_External", "Xe_External"},
+    const std::map<const G4String, const G4String> material_map = {
+    {"LXe", "G4_lXe"},
+    {"PTFE", "PTFE"},
+    {"Steel5", "Steel5"},
+    {"Vacuum", "G4_Galactic"},
+    {"GXe", "G4_Xe"},
+    {"Titanium", "G4_Ti"},
+    {"Air", "G4_AIR"},
+    // {"Rock", "StdRock"},
+    //{"Rock", "NaCl"},
+    {"Rock", "polyhalite"},
+    {"Water", "G4_WATER"},
+    {"GdLS", "GdLS"},
+    {"Steel", "G4_STAINLESS-STEEL"},
+    {"PE", "G4_POLYETHYLENE"},
+    {"Xe_External", "Xe_External"},
     };
 
 
